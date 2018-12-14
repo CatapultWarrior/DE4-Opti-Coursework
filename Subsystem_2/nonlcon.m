634384d787873bdee402127dfdea4ac86cc069ab
Catapult Warrior <45872801+CatapultWarrior@users.noreply.github.com>
2018-12-14 12:08:11 +0000

    Update nonlcon.m

--- Subsystem_2/nonlcon.m
+++ Subsystem_2/nonlcon.m
@@ -1,50 +1,51 @@
-%% Constraint Function
+%% Non linear Constraint Functions
 function [c,ceq]=nonlcon(x)
     %% Parameters
     %Ball properties
-    mb=1.0; %mass 
-    rb=0.108;%radius
+    mb=1.0; %Mass. Changed for parametric study
+    rb=0.108; %Radius.
     
     %Motor properties
-    Tmax=500; %Max torque
+    Tmax=500; %Max torque.
     
     %Beam properties
     d=0.001; %Maximum allowable deflection 1mm
-    mc=pi*x(5)^2*x(6)*x(4); %Mass of arm
-    I=1/3*mc*x(4)^2; %Moment of inertia of arm
+    mc=pi*x(4)^2*x(3)*x(2); %Mass of arm
+    I=1/3*mc*x(2)^2; %Moment of inertia of arm
     
-    %Oak Wood material properties. Oak was chosen because it has the
-    %highest yield strength and young's modulus (stiffness) among other
-    %natural materials. It is also low in density (lightweight) and cost.
-    %Material comparison charts are availible in github repo.
+    %Oak Wood material properties
+    %Oak was chosen because it has the highest yield strength and young's
+    %modulus in comparison to other natural materials. It is also low in
+    %cost and lightweight. Material Ashby plots are included in the github
+    %repo.
     YM=15.6*10^9;% Oak Wood Young's Modulus
     YS=55*10^6; %Oak Wood Yield Strength
     
     %Other parameters
-    g=9.81; %Gravitational Acceleration
-    t=0.04; %Duration of rotation
+    g=9.81;%gravitational acceleration
+    t=0.04;%duration of rotation
 
     
     %% Inequality Constraint Equations
-    %Maximum stress of the beam should be less than half its yield strength
-    g1= 2*(mb+mc)*g*x(4)/(pi*x(5)^3)-0.5*YS;
+    %Maximum stress of the beam
+    g1= 2*(mb+mc)*g*x(2)/(pi*x(4)^3)-0.5*YS;
     
     %Maxmimum mass of rod to be 10kg
-    g2= x(4)*x(6)*x(5)^2-10/pi; 
+    g2= x(2)*x(3)*x(4)^2-10/pi; 
     
     %Mimimum initial height 2m 
-    g3= -(x(4)*sin(x(1))-2); 
+    g3= -(x(2)*sin(x(1))-2); 
     
-    %Minimum speed of the motor must be 25 rads-1
+    %Minimum speed of the motor
     g4= -(x(1)/t-25);
     
-    %Maximum torque required must not exceed 500Nm
-    g6= (1/3*mc*x(4)^2+mb*x(4)^2)*(2*x(1)/t)-Tmax; 
+    %Maximum torque required must not exceed 
+    g6= (1/3*mc*x(2)^2+mb*x(2)^2)*(2*x(1)/t)-Tmax; 
     
-    %Maximum deflection of the beam must not exceed 0.001mm
-    g7=(mb*g*x(4)^3)/(3*YM*I)-d;
+    %Maximum deflection of the beam
+    g7=(mb*g*x(2)^3)/(3*YM*I)-d;
     
-    %Add only active non-linear constraints
+    %Active constraints
     c=[g1;g3;g4;g7];
     ceq=[];
 end
